4b7b0a2c4a8df6ce53420a922939c04a39ee15fc
Ben Yoshiwara <yoshiwara.b@gmail.com>
2013-11-05 21:28:12 -0500

    Added option handling to command line interface.

--- src/fileutils.py
+++ src/fileutils.py
@@ -6,36 +6,43 @@ from songidentity import SongInfo
 LAME_DECODE = "/course/cs4500f13/bin/lame --decode %s %s"
 
 def mp3_to_wave_subprocess(mp3):
-	"""convert mp3 to wave by calling lame in subprocess returns filename"""
-	wav = "%s.wav" % os.path.splitext(mp3)[0]
-	print wav
-	decode = LAME_DECODE % (mp3, wav)
-	print decode
-	subprocess.check_call(decode.split(" "))
-	return wav
+    """convert mp3 to wave by calling lame in subprocess returns filename"""
+    wav = "%s.wav" % os.path.splitext(mp3)[0]
+    print wav
+    decode = LAME_DECODE % (mp3, wav)
+    print decode
+    subprocess.check_call(decode.split(" "))
+    return wav
 
 
 def gen_file_list(dir):
-	"""Generates a list of all files in the given directory"""
-	filelist = []
-	for file in os.listdir(dir):
-		filelist.append(os.path.join(dir, file))
-	return filelist
+    """Generates a list of all files in the given directory"""
+    filelist = []
+    for file in os.listdir(dir):
+        filelist.append(os.path.join(dir, file))
+    return filelist
 
-def readfiles(filelist):
-	"""Records audio file info into SongInfo objects"""
-	song_info_list = []
-	for filename in filelist:
-		try:
-			song_info = SongInfo.from_file(filename)
-		except wave.Error:
-			print "%s is not in wave format, attempting to convert..." % filename
-			try:
-				wavefile = mp3_to_wave_subprocess(filename)
-				SongInfo.from_file(wavefile)
-			except subprocess.CalledProcessError as e:
-				print "Invalid file %s. Error: %s" % (filename, e)
-		if song_info:
-			song_info_list.append(song_info)
+def read_file(filename):
+    try:
+        song_info = SongInfo.from_file(filename)
+    except wave.Error:
+        print "%s is not in wave format, attempting to convert..." % filename
+        try:
+            wavefile = mp3_to_wave_subprocess(filename)
+            SongInfo.from_file(wavefile)
+        except subprocess.CalledProcessError as e:
+            print "Invalid file %s. Error: %s" % (filename, e)
+        except Exception as e:
+            print "Exception: %s" % e
+    if song_info:
+        return song_info
 
-	return song_info_list
+
+def read_files(filelist):
+    """Records audio file info into SongInfo objects"""
+    song_info_list = []
+    for filename in filelist:
+        song_info = read_file(filename)
+        if song_info:
+            song_info_list.append(song_info)
+    return song_info_list
--- src/songidentity.py
+++ src/songidentity.py
@@ -6,11 +6,12 @@ import sys
 import wave
 from array import array
 
-class SongInfo():
+class SongInfo(object):
     """Class to encapsulate song data and info."""
 
     @classmethod
     def from_file(self, filename):
+        """Create song_info object from wave file"""
         wavefile = wave.open(filename, 'r')
         nframes = wavefile.getnframes()
         nchannels = wavefile.getnchannels()
@@ -21,23 +22,32 @@ class SongInfo():
         return song_info
     
     def __init__(self, filename, nchannels):
+        """Constructor for SongInfo.
+        filename is a string indicating a path to a wave file.
+        nchannels is the number of channels the wave file contains.
+        """
         self.name = self.get_name(filename)
         self.nchannels = nchannels
         self.wave_integer_array = None
         self.matches = []
 
     def get_name(self, filepath):
+        """Given a full filepath, returns just the wave file name"""
         pathlist = filepath.split('/')
         name = pathlist[len(pathlist)-1]
         return name
 
     def compare_bytestring(self, song_info):
-        """Compares song"""
+        """Compares song bytestrings directly. Adds song being compared
+        to self.matches if there is an exact match.
+        """
         if song_info.bytestring == self.bytestring:
             self.matches.append(song_info.name)
 
     def byte_string_to_integer_array(self, bytestring):
-        """Converts output string from wave.readframes into a signed integer array"""
+        """Converts output bytestring from wave.readframes into a 
+        signed integer array
+        """
         raw_array = array('h', bytestring)
         if self.nchannels == 2:
             left = raw_array[0::2]
@@ -48,7 +58,9 @@ class SongInfo():
         self.wave_integer_array = mono
 
     def compare(self, song_info):
-        """If the files are the same lenght do a numpy correlation check otherwise string search."""
+        """If the files are the same length do a numpy correlation 
+        check otherwise string search.
+        """
         if len(self.wave_integer_array) == len(song_info.wave_integer_array):
             correlation_matrix = np.corrcoef(self.wave_integer_array,
                                                 song_info.wave_integer_array)
@@ -57,21 +69,3 @@ class SongInfo():
         elif (self.wave_integer_array in song_info.wave_integer_array or
               song_info.wave_integer_array in self.wave_integer_array):
             print "MATCH: %s %s" % (self.name, song_info.name)
-
-def compare_song_info_lists(song_info_list1, song_info_list2):
-    for song1 in song_info_list1:
-        for song2 in song_info_list2:
-            if song1 and song2:
-                song1.compare(song2)
-
-def main(argv):
-    dir1 = argv[1]
-    dir2 = argv[2]
-    filelist1 = fileutils.gen_file_list(dir1)
-    filelist2 = fileutils.gen_file_list(dir2)
-    song_info_list1 = fileutils.readfiles(filelist1)
-    song_info_list2 = fileutils.readfiles(filelist2)
-    compare_song_info_lists(song_info_list1, song_info_list2)
-
-if __name__ == '__main__':
-    main(sys.argv)
